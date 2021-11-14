c33d6b996f2803f3636740721d2b5968f75ed8a5
Hyunjoo-5584824 <64509600+Hyunjoo-5584824@users.noreply.github.com>
2021-11-15 02:18:49 +0900

    sign_in Design

--- signIn.Designer.cs
+++ signIn.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Drawing;
+
 namespace DB_SNS
 {
     partial class signIn
@@ -7,18 +9,20 @@ namespace DB_SNS
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
         /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
+           
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
             base.Dispose(disposing);
+            
         }
 
         #region Windows Form 디자이너에서 생성한 코드
@@ -29,80 +33,157 @@ namespace DB_SNS
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_id = new System.Windows.Forms.TextBox();
-            this.textBox_pw = new System.Windows.Forms.TextBox();
-            this.button_login = new System.Windows.Forms.Button();
             this.checkBox_Remember = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_pw = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button_login = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.singUpButton = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // textBox_id
-            // 
-            this.textBox_id.Location = new System.Drawing.Point(121, 138);
-            this.textBox_id.Name = "textBox_id";
-            this.textBox_id.Size = new System.Drawing.Size(100, 25);
-            this.textBox_id.TabIndex = 0;
-            // 
-            // textBox_pw
-            // 
-            this.textBox_pw.Location = new System.Drawing.Point(121, 195);
-            this.textBox_pw.Name = "textBox_pw";
-            this.textBox_pw.Size = new System.Drawing.Size(100, 25);
-            this.textBox_pw.TabIndex = 1;
-            // 
-            // button_login
-            // 
-            this.button_login.Location = new System.Drawing.Point(40, 281);
-            this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(75, 23);
-            this.button_login.TabIndex = 2;
-            this.button_login.Text = "LogIN";
-            this.button_login.UseVisualStyleBackColor = true;
-            this.button_login.Click += new System.EventHandler(this.button_login_Click);
             // 
             // checkBox_Remember
             // 
             this.checkBox_Remember.AutoSize = true;
-            this.checkBox_Remember.Location = new System.Drawing.Point(129, 285);
+            this.checkBox_Remember.Font = new System.Drawing.Font("나눔바른고딕 Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox_Remember.Location = new System.Drawing.Point(123, 609);
+            this.checkBox_Remember.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox_Remember.Name = "checkBox_Remember";
-            this.checkBox_Remember.Size = new System.Drawing.Size(156, 19);
+            this.checkBox_Remember.Size = new System.Drawing.Size(119, 24);
             this.checkBox_Remember.TabIndex = 3;
-            this.checkBox_Remember.Text = "Remember Account";
+            this.checkBox_Remember.Text = "자동 로그인";
             this.checkBox_Remember.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 142);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 469);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.Size = new System.Drawing.Size(25, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 198);
+            this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 550);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.Size = new System.Drawing.Size(100, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "PASSWORD";
             // 
-            // Form1
+            // panel3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.panel3.BackgroundImage = global::on_off_proj.Properties.Resources.ID_Pass6;
+            this.panel3.Controls.Add(this.textBox_id);
+            this.panel3.Controls.Add(this.textBox_pw);
+            this.panel3.Location = new System.Drawing.Point(123, 448);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(355, 141);
+            this.panel3.TabIndex = 1;
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.textBox_id.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_id.Location = new System.Drawing.Point(55, 21);
+            this.textBox_id.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(276, 21);
+            this.textBox_id.TabIndex = 0;
+            // 
+            // textBox_pw
+            // 
+            this.textBox_pw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.textBox_pw.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_pw.Location = new System.Drawing.Point(55, 101);
+            this.textBox_pw.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_pw.Name = "textBox_pw";
+            this.textBox_pw.Size = new System.Drawing.Size(276, 21);
+            this.textBox_pw.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BackgroundImage = global::on_off_proj.Properties.Resources.사진;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 374);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::on_off_proj.Properties.Resources.온오프프1;
+            this.panel2.Location = new System.Drawing.Point(252, 291);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 80);
+            this.panel2.TabIndex = 0;
+            // 
+            // button_login
+            // 
+            this.button_login.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.button_login.FlatAppearance.BorderSize = 2;
+            this.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_login.Location = new System.Drawing.Point(366, 736);
+            this.button_login.Margin = new System.Windows.Forms.Padding(4);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(119, 70);
+            this.button_login.TabIndex = 2;
+            this.button_login.Text = "On";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // singUpButton
+            // 
+            this.singUpButton.BackColor = System.Drawing.Color.Transparent;
+            this.singUpButton.FlatAppearance.BorderSize = 0;
+            this.singUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.singUpButton.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.singUpButton.Location = new System.Drawing.Point(366, 602);
+            this.singUpButton.Name = "singUpButton";
+            this.singUpButton.Size = new System.Drawing.Size(108, 35);
+            this.singUpButton.TabIndex = 9;
+            this.singUpButton.Text = "회원가입";
+            this.singUpButton.UseVisualStyleBackColor = false;
+            // 
+            // signIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 653);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(516, 844);
+            this.Controls.Add(this.singUpButton);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox_Remember);
             this.Controls.Add(this.button_login);
-            this.Controls.Add(this.textBox_pw);
-            this.Controls.Add(this.textBox_id);
-            this.Name = "Form1";
-            this.Text = "SNS";
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(538, 1800);
+            this.Name = "signIn";
+            this.Text = "On-off";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +197,11 @@ namespace DB_SNS
         private System.Windows.Forms.CheckBox checkBox_Remember;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button singUpButton;
     }
 }
 
